bba4582e0953aedee06eff7fa5e69df07d79fece
matiasagbenitez <matiasagbenitez@gmail.com>
2025-02-14 08:27:41 -0300

    fonts config

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 
 const layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
-    <main className="font-work-sans">
+    <main className="--font-work-sans">
       <Navbar />
       {children}
     </main>
