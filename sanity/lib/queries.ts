444aa5d89a75118361dea351b3a0c7fc50f769d1
matiasagbenitez <matiasagbenitez@gmail.com>
2025-02-14 17:41:46 -0300

    startup details

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -17,3 +17,28 @@ export const STARTUPS_QUERY = defineQuery(`
     category, 
     image
 }`);
+
+export const STARTUP_BY_ID_QUERY = defineQuery(`
+*[_type == 'startup' && _id == $id][0]{
+  _id, 
+  title, 
+  slug, 
+  _createdAt, 
+  author -> {
+    _id,
+    name,
+    username,
+    image,
+    bio
+  },
+  views, 
+  description, 
+  category, 
+  image,
+  pitch
+}`);
+
+export const STARTUP_VIEWS_QUERY = defineQuery(`
+*[_type == 'startup' && _id == $id][0]{
+  _id, views
+}`);
