2a60f4bf28d9c20507482a6d323abf4e6b7223da
matiasagbenitez <matiasagbenitez@gmail.com>
2025-02-14 17:53:52 -0300

    write client

--- components/View.tsx
+++ components/View.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
 import Ping from "./Ping";
+import { writeClient } from "@/sanity/lib/write-client";
+import { after } from "next/server";
 
 const View = async ({ id }: { id: string }) => {
   //   const { views: totalViews } = await client.fetch(STARTUP_VIEWS_QUERY, { id });
@@ -9,6 +11,13 @@ const View = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client
     .withConfig({ useCdn: false })
     .fetch(STARTUP_VIEWS_QUERY, { id });
+
+  after(async () => {
+    await writeClient
+      .patch(id)
+      .set({ views: totalViews + 1 })
+      .commit();
+  });
 
   return (
     <div className="view-container border border-black-300 rounded-lg relative">
