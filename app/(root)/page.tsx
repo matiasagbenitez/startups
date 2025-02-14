7be249e08e5d74345dafeb6315246364e50324f2
matiasagbenitez <matiasagbenitez@gmail.com>
2025-02-14 08:39:28 -0300

    hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect with Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches and Get Noticied in Virtual Competitions
+        </p>
+      </section>
     </div>
   );
 }
